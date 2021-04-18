2bb2de4df4bb35c08487bbf6e89d898caad6c128
Angel-68 <69690400+Angel-68@users.noreply.github.com>
2021-04-18 17:12:51 +0200

    Update HolaMundo.java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,7 +1,7 @@
 import otrasClases.Animal;
 import otrasClases.Persona;
 
-// En Java un programa es un conjunto de definiciones de clases que están dispuestas en uno o más archivos.
+// En Java un programa es un conjunto de definiciones de clases que estÃ¡n dispuestas en uno o mÃ¡s archivos.
 
 // Este es mi primer programa Java formado por una sola class llamada HolaMundo
 
@@ -9,20 +9,21 @@ import otrasClases.Persona;
 
 // Los comentarios en java son cualquier texto despues de dos barras
 
-/* Java tambien admite comentarios multilínea como en C
-   Otra línea del comentario
+/* Java tambien admite comentarios multilÃ­nea como en C
+   Otra lÃ­nea del comentario
+   Otra lÃ­nea
  */
 
 public class HolaMundo {
 
 	public static void main(String[] args) {
-	// Método main, es el punto de entrada a la aplicación java, por eso se llama "principal" es el primer método que se ejecuta
-	// La forma (String[] args) es la definición de los argumentos que recibe el método main. 
-	// En este caso se recibe un  solo argumento llamado args. Los paréntesis [] indican que el argumento es un arreglo o array y la palabra String indica que cada elemento del array es una cadena de caracteres.
+	// MÃ©todo main, es el punto de entrada a la aplicaciÃ³n java, por eso se llama "principal" es el primer mÃ©todo que se ejecuta
+	// La forma (String[] args) es la definiciÃ³n de los argumentos que recibe el mÃ©todo main. 
+	// En este caso se recibe un  solo argumento llamado args. Los parÃ©ntesis [] indican que el argumento es un arreglo o array y la palabra String indica que cada elemento del array es una cadena de caracteres.
     
 		System.out.println("Hola Mundo");
 		
-		 if (args.length > 1) { //si hay más de 1 argumento
+		 if (args.length > 1) { //si hay mÃ¡s de 1 argumento
 	            System.out.println("Hay demasiados argumentos. Debe escribir: HolaMundo nombre");
 	        } else if (args.length == 0) { //si no hay argumentos      
 	            System.out.println("Hola Mundo");
@@ -51,21 +52,21 @@ public class HolaMundo {
 
 }
 
-/* Para ejecutar esta clase desde línea de comandos:
-	1º Abrimos un editor de línea de comandos, podemos usar el Símbolo del sistema (CMD) o el Power Shell.
-	2º En ambos estaremos colocados en la carpeta (Directorio) de nuestro usuario, en el ordenador ASUS será C:\Users\bartolin>
-	3º Usando los comandos DIR (ver el contenido del directorio) y CD (CambioDirectirio) nis vamos hasta el lugar donde está el archivo HolaMundo.java
-	   en el ASUS está en D:\Datos Alba\INGENIERIA_MATEMATICA\HERRAMIENTAS\CURSO_JAVA>
-	4º Ya colocados en D:\Datos Alba\INGENIERIA_MATEMATICA\HERRAMIENTAS\CURSO_JAVA>
+/* Para ejecutar esta clase desde lÃ­nea de comandos:
+	1Âº Abrimos un editor de lÃ­nea de comandos, podemos usar el SÃ­mbolo del sistema (CMD) o el Power Shell.
+	2Âº En ambos estaremos colocados en la carpeta (Directorio) de nuestro usuario, en el ordenador ASUS serÃ¡ C:\Users\bartolin>
+	3Âº Usando los comandos DIR (ver el contenido del directorio) y CD (CambioDirectirio) nis vamos hasta el lugar donde estÃ¡ el archivo HolaMundo.java
+	   en el ASUS estÃ¡ en D:\Datos Alba\INGENIERIA_MATEMATICA\HERRAMIENTAS\CURSO_JAVA>
+	4Âº Ya colocados en D:\Datos Alba\INGENIERIA_MATEMATICA\HERRAMIENTAS\CURSO_JAVA>
 		4.1 Compilamos D:\Datos Alba\INGENIERIA_MATEMATICA\HERRAMIENTAS\CURSO_JAVA>javac HolaMUndo.java
-		4.2 Si no ha dado errores se habrá creado un nuevo archivo HolaMundo.class (podemos comprobarlo con un DIR)
+		4.2 Si no ha dado errores se habrÃ¡ creado un nuevo archivo HolaMundo.class (podemos comprobarlo con un DIR)
 		4.3 Ejecutamos la clase HolaMundo.class D:\Datos Alba\INGENIERIA_MATEMATICA\HERRAMIENTAS\CURSO_JAVA>java HolaMundo
 		4.4 Si queremos pasar argumentos los escribimos detras del nombre de la clase D:\Datos Alba\INGENIERIA_MATEMATICA\HERRAMIENTAS\CURSO_JAVA>java HolaMundo Alba
 	*/
 /* Para ejecutar esta clase desde el IDE Eclipse:
 	Hacemos clic en la flecha verde de la barra de herramientas "Run HolaMundo", si no hemos guardado el archivo HolaMundo.java primero lo guarda.
 	Si queremos pasarle argumentos, desplegamos la flecha negra junto al "Run HolaMundo" y elegimos "Run Configurations"
-	Se abre un formulario, pinchamos en la pestaña "Arguments"
-	En el campo "Program Arguments" escribimos los argumentos separados por un espacio y clicamos en el botón "RUN"
+	Se abre un formulario, pinchamos en la pestaÃ±a "Arguments"
+	En el campo "Program Arguments" escribimos los argumentos separados por un espacio y clicamos en el botÃ³n "RUN"
 	Estos argumentos quedan grabados, si no los queremos en otras ejecuciones solo con flecha verde hay que borrarlos de ese campo
 */
